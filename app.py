e1d148a5bfca483fbde949794abeb3260fddf945
Stefan Dragomir <s.dragomir@t-online.de>
2023-04-23 20:02:35 +0200

    update app

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def home():
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-# TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
     # Performs an sklearn prediction
@@ -42,6 +41,7 @@ def predict():
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
+    LOG.info("Predicted value is: %s", prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
